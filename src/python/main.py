dce20b0fd459782b97ba54a05ed46dff11fe017b
LayBraid <clement.loeuillet@hotmail.com>
2022-08-11 19:27:48 +0200

    add small patch

--- src/python/main.py
+++ src/python/main.py
@@ -1,25 +1,20 @@
 import numpy as np
 import torch
-import csv
-
 
 import torch.optim as optim
-import torch as tor
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-class model(nn.Module):
-    def __init__(self, X):
-        super(model, self).__init__()
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
         self.fc1 = nn.Linear(3, 1)
         self.data = []
         self.model = torch.nn.Sequential(
-            torch.nn.Linear(3, 1),
+            torch.nn.Linear(3, 2),
             torch.nn.Flatten(0, 1)
         )
-        self.Y = torch.sin(X)
-
 
     def forward(self, X):
         y_pred = self.model(X)
@@ -30,15 +25,12 @@ test = torch.randn(1, 3)
 
 print(test)
 
-nnmodel = model(torch.linspace(0, 2038602856570, steps=2))
+nnmodel = Model()
 optimizer = optim.Adam(nnmodel.parameters(), lr=0.01)
 
 output = nnmodel.fc1(test)
 
 print(output)
-
-
-
 
 # file = open("blocks.json")
 
@@ -51,8 +43,6 @@ print(output)
 #     i += 1
 #     # print(transaction)
 #     print(transaction["gas_used"], transaction["gas_price"], sep=",")
-
-
 
 
 np_data = np.loadtxt("new_gas_data.csv", dtype=np.float32, delimiter=",", skiprows=1)
@@ -84,21 +74,22 @@ def test(weights, data_test):
         correct_predictions = prediction.eq(target.view_as(prediction)).sum().item()
         correct += correct_predictions
     accuracy = correct / test_size
-    print(f"accuracy: on test: {accuracy*100}%")
+    print(f"accuracy: on test: {accuracy * 100}%")
 
 
 it = 0
-loss = nn.CrossEntropyLoss()
+loss = nn.BCEWithLogitsLoss()
 for data in mev_dataset:
 
-    if weights.grad is not None:
-        weights.grad.zero_()
-        optimizer.zero_grad()
+    ## if weights.grad is not None:
+    ##    weights.grad.zero_()
+    ##    optimizer.zero_grad()
 
     data = data.view((-1, 3))
     target = data[0][2]
     data = nnmodel.forward(data)
     output = loss(data, torch.tensor(0.2).long())
+    optimizer.zero_grad()
     output.backward()
     optimizer.step()
 
